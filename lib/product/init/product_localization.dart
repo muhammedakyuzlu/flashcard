5a09f77181f58f6274f9c37ee80f5f9747bb320b
muhammed akyuzlu <muhammedakyuzlu6@gmail.com>
2025-04-30 20:58:01 +0300

    no code changed

--- lib/product/init/product_localization.dart
+++ lib/product/init/product_localization.dart
@@ -6,12 +6,22 @@ import 'package:flutter/material.dart';
 /// Product localization manager
 final class ProductLocalization extends EasyLocalization {
   /// ProductLocalization need to [child] for a wrap locale item
-  ProductLocalization({required super.child, super.key}) : super(supportedLocales: _supportedItems, path: _translationPath, useOnlyLangCode: true);
+  ProductLocalization({required super.child, super.key})
+    : super(
+        supportedLocales: _supportedItems,
+        path: _translationPath,
+        useOnlyLangCode: true,
+      );
 
-  static final List<Locale> _supportedItems = [Locales.en.locale];// if you want to add a new locale, please add it here
+  static final List<Locale> _supportedItems = [
+    Locales.en.locale,
+  ]; // if you want to add a new locale, please add it here
 
   static const String _translationPath = 'assets/translations';
 
   /// Change project language by using [Locales]
-  static Future<void> updateLanguage({required BuildContext context, required Locales value}) => context.setLocale(value.locale);
+  static Future<void> updateLanguage({
+    required BuildContext context,
+    required Locales value,
+  }) => context.setLocale(value.locale);
 }
