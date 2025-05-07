b44695f45c10bee42ce611f5579d4908c5b398e1
muhammed akyuzlu <muhammedakyuzlu6@gmail.com>
2025-05-07 03:06:01 +0300

    supporting responsive

---
+++ lib/development/preview_main.dart
@@ -0,0 +1,40 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flashcard/product/init/application_initialize.dart';
+import 'package:flashcard/product/init/product_localization.dart';
+import 'package:flashcard/product/init/theme/custom_dark_theme.dart';
+import 'package:flashcard/product/init/theme/custom_light_theme.dart';
+import 'package:flashcard/product/navigation/app_router.dart';
+import 'package:flutter/material.dart';
+import 'package:widgets/widgets.dart';
+
+Future<void> main() async {
+  await ApplicationInitialize().make();
+  runApp(
+    DevicePreview(
+      // builder:
+      //     (context) => StateInitialize(
+      //       child: ProductLocalization(child: const _MyApp()),
+      //     ),
+      builder: (context) => ProductLocalization(child: const _MyApp()),
+    ),
+  );
+}
+
+final class _MyApp extends StatelessWidget {
+  const _MyApp();
+  static final _appRouter = AppRouter();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: _appRouter.config(),
+      builder: CustomResponsive.build,
+      theme: CustomLightTheme().themeData,
+      darkTheme: CustomDarkTheme().themeData,
+      // themeMode: context.watch<ProductViewModel>().state.themeMode,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+    );
+  }
+}
